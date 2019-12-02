5740b491015789659b86a9bae4c6e6b465fff86a
lukassurkevicius <lukassurkevicius@gmail.com>
2019-12-02 14:51:41 +0000

    test

--- HelloPython.py
+++ HelloPython.py
@@ -3,6 +3,7 @@ print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
+print("Adding test line")
 print("Adding test line")	
 
 
