e234c9e5b04127e1341a76549bae37804addfcaa
Adam Carroll <adamcarroll99@mycit.ie>
2019-12-06 11:20:11 +0000

    test123

--- HelloPython.py
+++ HelloPython.py
@@ -4,6 +4,4 @@ now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
 print("Adding test line")
-dsadsa
-
-	
+print("Testing...testing...123")
